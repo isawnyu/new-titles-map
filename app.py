3e2b6ba3e663350ce35e46148089c55bc55169e0
Patrick J. Burns <patrick@diyclassics.org>
2018-02-21 14:49:35 -0500

    Update routes

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, jsonify
+from sqlalchemy import extract
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -11,9 +12,28 @@ db = SQLAlchemy(app)
 
 from models import NewTitles
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route('/')
 def index():
-    return render_template('index.html')
+    cols = ['bsn', 'latitude', 'longitude']
+    data = NewTitles.query.all()
+    result = [{col: str(getattr(d, col)) for col in cols} for d in data]
+    return render_template('index.html', result=result)
+
+@app.route('/<year>')
+def titles_by_year(year):
+    cols = ['bsn', 'latitude', 'longitude', 'date']
+    data = NewTitles.query.filter(extract('year', NewTitles.date) == year).all()
+    result = [{col: str(getattr(d, col)) for col in cols} for d in data]
+    return render_template('index.html', result=result)
+
+
+@app.route('/<year>/<month>')
+def titles_by_month(year, month):
+    cols = ['bsn', 'latitude', 'longitude', 'date']
+    data = NewTitles.query.filter(extract('year', NewTitles.date) == year).filter(extract('month', NewTitles.date) == month).all()
+    result = [{col: str(getattr(d, col)) for col in cols} for d in data]
+    return render_template('index.html', result=result)
 
 
 if __name__ == '__main__':
